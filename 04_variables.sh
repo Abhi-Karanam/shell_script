076e545f3a69fc1dfa806aeb0ce1b001189c5144
KING'S LAPTOP <iamabhi.karanam@gmail.com>
2025-02-02 17:32:04 +0530

    added runtime variables file

---
+++ 04_variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PERSON1=$1
+PERSON2=$2
+
+echo "$PERSON1::Hi $PERSON2, How are you?"
+echo "$PERSON2::Hello $PERSON1 I'm Good.What about you?"
+echo "$PERSON1::Okay $PERSON2, Thank you"
+echo "$PERSON2::You are welcome,Bye $PERSON1"
