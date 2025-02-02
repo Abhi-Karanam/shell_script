06a16e74b8c5f586aaf1b0ac202a8a5dd23c508c
KING'S LAPTOP <iamabhi.karanam@gmail.com>
2025-02-02 16:53:38 +0530

    variables & conversation added

---
+++ 02-variables.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+echo "Abhi::Hi Nishanth, How are you?"
+echo "Nishanth::Hello Abhi, I'm Good.What about you?"
+echo "Abhi::Okay Nishanth, Thank you"
+echo "Nishanth::You are welcome,Bye Abhi"
