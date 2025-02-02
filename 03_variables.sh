ed30fb6446801d55bdbc105f38824b486357a0a2
KING'S LAPTOP <iamabhi.karanam@gmail.com>
2025-02-02 17:27:25 +0530

    New variable added

---
+++ 03_variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PERSON1=Abhi
+PERSON2=Nishanth
+
+echo "$PERSON1::Hi $PERSON2, How are you?"
+echo "$PERSON2::Hello $PERSON1 I'm Good.What about you?"
+echo "$PERSON1::Okay $PERSON2, Thank you"
+echo "$PERSON2::You are welcome,Bye $PERSON1"
