91f352bcd5cd45449b59b7e603dd3516633e3cd7
KING'S LAPTOP <iamabhi.karanam@gmail.com>
2025-02-05 08:31:47 +0530

    Arrays script added

--- 07_arrays.sh
+++ 07_arrays.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-MOVIES=${[OG, PUSHPA, Game Changer]}
+MOVIES=$("OG", "PUSHPA", "Game Changer")
 
 echo "First movie is: ${MOVIES[1]}"
+echo "Seconf movie is: ${MOVIES[2]}"
+echo "Third movie is:${MOVIES[3]}"
