27bf3b5c18aa9eb18ae834ebf4c73cc9ae226f1d
KING'S LAPTOP <iamabhi.karanam@gmail.com>
2025-02-08 12:09:46 +0530

    Arrays script edited

---
+++ 09_conditions.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+NUMBER=$1
+
+if [ $NUMBER -gt 100 ]
+then
+    echo "Given number is greater than 100"
+
+if [ $NUMBER -eq 100 ]
+then
+    echo "Given number is equal to 100"
+    
+else
+    echo "Given number is less than 100"
+fi 
