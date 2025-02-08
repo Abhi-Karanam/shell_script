02ccb6df065f644c638448d650f23169424ea5c0
KING'S LAPTOP <iamabhi.karanam@gmail.com>
2025-02-08 11:37:28 +0530

    Arrays script edited

--- 08_special_vars.sh
+++ 08_special_vars.sh
@@ -3,8 +3,9 @@
 echo "All Variables: $@"
 echo "Number of Variables: $#"
 echo "Present working directory: $pwd"
-echo "script nmae: $0"
+echo "script name: $0"
 echo "home directory of current user: $HOME"
 echo "Which User is running the script: $USER"
 echo "process id of current script: $$"
-echo "process id of last command: $!
+sleep 60 &
+echo "process id of last command: $!"
