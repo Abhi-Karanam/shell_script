70723466d8a7e22c7256118afbd896a4a463fa49
KING'S LAPTOP <iamabhi.karanam@gmail.com>
2025-02-09 17:33:23 +0530

    Installation script added

--- install_script.sh
+++ install_script.sh
@@ -4,6 +4,15 @@ USERID=$(id -u)
 if [ $USERID -ne 0 ]
 then
     echo "ERROR:: You must have sudo access to execute the script"
+    exit 1 
 fi
 
 dnf install mysql -y
+if [ $? -ne 0 ]
+then    
+    echo "Installing MySQL ...FAILURE"
+    exit 1
+else
+    echo "Installing MySQL ...SUCCESS"
+fi
+
